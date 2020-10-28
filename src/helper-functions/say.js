50f6a76af4383b74bd5236ac3ea2b8770a23606b
pollmix <polboy777@gmail.com>
2020-10-28 18:18:07 +0600

    added second say function

--- src/helper-functions/say.js
+++ src/helper-functions/say.js
@@ -1,3 +1,30 @@
 export default function say(word) {
-  window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
+  if ("speechSynthesis" in window) {
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
+  } else {
+    say2(word);
+  }
+}
+
+function say2(word) {
+  let ctx = new AudioContext();
+  let audio;
+
+  fetch(
+    `http://api.voicerss.org/?key=aebe03bb99db41169c32a57571fe577e&hl=en-us&v=Mary&c=MP3&src=${word}`
+  )
+    .then((data) => data.arrayBuffer())
+    .then((arrayBuffer) => ctx.decodeAudioData(arrayBuffer))
+    .then((decodeAudioData) => {
+      audio = decodeAudioData;
+    })
+    .then(() => {
+      let playSound = ctx.createBufferSource();
+      playSound.buffer = audio;
+      playSound.connect(ctx.destination);
+      playSound.start(ctx.currentTime);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
